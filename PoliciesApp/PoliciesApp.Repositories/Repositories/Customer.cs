a2fbaaba9442c11cd87f7b30aa012cef86984f0d
Heider Llantén <heiderllanten@outlook.com>
2019-07-03 19:19:35 -0500

    Implementing repository pattern

---
+++ PoliciesApp/PoliciesApp.Repositories/Repositories/Customer.cs
@@ -0,0 +1,16 @@
+﻿using PoliciesApp.Entities.Entities;
+using PoliciesApp.Repositories.Infraestructure;
+using System.Data.Entity;
+
+namespace PoliciesApp.Repositories.Repositories
+{
+    public interface ICustomerRepository : IGenericRepository<Customer>
+    { }
+
+    public class CustomerRepository : GenericRepository<Customer>
+    {
+        public CustomerRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ PoliciesApp/PoliciesApp.Repositories/Repositories/Policy.cs
@@ -0,0 +1,16 @@
+﻿using PoliciesApp.Entities.Entities;
+using PoliciesApp.Repositories.Infraestructure;
+using System.Data.Entity;
+
+namespace PoliciesApp.Repositories.Repositories
+{
+    public interface IPolicyRepository : IGenericRepository<Policy>
+    { }
+
+    public class PolicyRepository : GenericRepository<Policy>
+    {
+        public PolicyRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
